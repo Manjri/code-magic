00a8b3d79f3c4f39b72ad885e9d708d3fee93bca
njagdale <njagdale@gmail.com>
2015-10-09 11:52:30 -0700

    Hello World
    Simple Hello World

---
+++ HelloWorld/HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Nikhil Jagdale on 5/23/15.
+//  Copyright (c) 2015 Nikhil Jagdale. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
