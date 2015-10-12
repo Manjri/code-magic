2152122e1c896732891a510e74675bbdf2d01cf0
njagdale <njagdale@gmail.com>
2015-10-12 12:16:37 -0700

    update

--- puzzles/LL_RemoveMultipleOccurence_Elem/main.cpp
+++ puzzles/LL_RemoveMultipleOccurence_Elem/main.cpp
@@ -94,3 +94,28 @@ int main()
     printList(head);
     return 0;
 }
+
+
+/**
+// Short Recursive Solution in C++
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        
+        if(!head)
+            return NULL;
+        
+        head->next = removeElements(head->next, val);
+        return (head->val == val) ? (head->next) : head;
+    }
+};
+
+*/
