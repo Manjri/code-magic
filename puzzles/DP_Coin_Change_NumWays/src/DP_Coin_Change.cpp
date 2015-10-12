874a01a2780af8765ebbadaf5e407a4d32bd73fd
njagdale <njagdale@gmail.com>
2015-10-11 18:49:16 -0700

    update

--- puzzles/DP_Coin_Change_NumWays/src/DP_Coin_Change.cpp
+++ puzzles/DP_Coin_Change_NumWays/src/DP_Coin_Change.cpp
@@ -1,18 +1,17 @@
-#if 0
 #include<stdio.h>
 
 int count( int S[], int m, int n )
 {
     int i, j, x, y;
-
+    
     // We need n+1 rows as the table is consturcted in bottom up manner using
     // the base case 0 value case (n = 0)
     int table[n+1][m];
-
-    // Fill the enteries for 0 value case (n = 0)
+    
+    // Fill the entries for 0 value case (n = 0)
     for (i=0; i<m; i++)
         table[0][i] = 1;
-
+    
     // Fill rest of the table enteries in bottom up manner
     for (i = 1; i < n+1; i++)
     {
@@ -20,10 +19,10 @@ int count( int S[], int m, int n )
         {
             // Count of solutions including S[j]
             x = (i-S[j] >= 0)? table[i - S[j]][j]: 0;
-
+            
             // Count of solutions excluding S[j]
             y = (j >= 1)? table[i][j-1]: 0;
-
+            
             // total count
             table[i][j] = x + y;
         }
@@ -34,32 +33,36 @@ int count( int S[], int m, int n )
 // Driver program to test above function
 int main()
 {
-    int arr[] = {2, 5, 3, 6};
+    //int arr[] = {2, 5, 3, 6};
+    int arr[] = {1, 2, 3};
     int m = sizeof(arr)/sizeof(arr[0]);
-    int n = 10;
+    int n = 4;
     printf(" %d ", count(arr, m, n));
     return 0;
 }
-#endif
+
+#if 0
+
+// This is the non-DP (recursive solution)
 
 #include<stdio.h>
 
 // Returns the count of ways we can sum  S[0...m-1] coins to get sum n
 int count( int S[], int m, int n )
 {
-	printf("\n m - %d n - %d",m,n);
+    //printf("\n m - %d n - %d",m,n);
     // If n is 0 then there is 1 solution (do not include any coin)
     if (n == 0)
         return 1;
-
+    
     // If n is less than 0 then no solution exists
     if (n < 0)
         return 0;
-
+    
     // If there are no coins and n is greater than 0, then no solution exist
     if (m <=0 && n >= 1)
         return 0;
-
+    
     // count is sum of solutions (i) including S[m-1] (ii) excluding S[m-1]
     return count( S, m - 1, n ) + count( S, m, n-S[m-1] );
 }
@@ -74,3 +77,4 @@ int main()
     getchar();
     return 0;
 }
+#endif
