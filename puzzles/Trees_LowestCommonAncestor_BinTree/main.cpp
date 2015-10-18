3d638583858fcb053157eaa2ef4d98de5a778bc4
njagdale <njagdale@gmail.com>
2015-10-17 20:51:34 -0700

    update

---
+++ puzzles/Trees_LowestCommonAncestor_BinTree/main.cpp
@@ -0,0 +1,29 @@
+// DFS based solution
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if((root==NULL) || (root == p) || (root == q))
+            return root;
+        
+        TreeNode* parent1 = lowestCommonAncestor(root->left, p, q);
+        TreeNode* parent2 = lowestCommonAncestor(root->right, p, q);
+        
+        if(parent1 && parent2)
+            return root;
+        
+        return parent1 ? parent1: parent2;
+        
+    }
+};
