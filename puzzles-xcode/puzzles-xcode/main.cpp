c45af6ef1f3375fb4b0023c074172c0a2e146836
njagdale <njagdale@gmail.com>
2015-10-18 19:16:38 -0700

    update

--- puzzles-xcode/puzzles-xcode/main.cpp
+++ puzzles-xcode/puzzles-xcode/main.cpp
@@ -1,132 +1,99 @@
-// A Backtracking program  in C++ to solve Sudoku problem
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-// UNASSIGNED is used for empty cells in sudoku grid
-#define UNASSIGNED 0
 
-// N is used for size of Sudoku grid. Size will be NxN
-#define N 9
+/**
+ 
+ 1-2-3
+ 
+ Initially we have 3 choices: 1,2,3 we choose 1 and go
+ At 1, we have 2 choices: 2,3, we choose 2 and go
+ At 2, we have 1 choice only: 3
+ At 3 we are done - so 3 is explored
+ Backtrack to 2, we didnt have any other choice other and 3 - so 2 is explored
+ Backtrack to 1, we now explore 3 and go
+ After visiting 3, we are done - so 3 is explored
+ Backtrack back to 1 - our choices 2,3 are explored and so 1 is also explored.
+ 
+ Backtrack to initial position and move with our next choice 2... and move ahead from 2
+ story continues similarly...
+ 
+ */
 
-// This function finds an entry in grid that is still unassigned
-bool FindUnassignedLocation(int grid[N][N], int &row, int &col);
 
-// Checks whether it will be legal to assign num to the given row,col
-bool isSafe(int grid[N][N], int row, int col, int num);
 
-/* Takes a partially filled-in grid and attempts to assign values to
- all unassigned locations in such a way to meet the requirements
- for Sudoku solution (non-duplication across rows, columns, and boxes) */
-bool SolveSudoku(int grid[N][N])
-{
-    int row, col;
+class Solution {
+public:
     
-    // If there is no unassigned location, we are done
-    if (!FindUnassignedLocation(grid, row, col))
-        return true; // success!
-    
-    // consider digits 1 to 9
-    for (int num = 1; num <= 9; num++)
-    {
-        // if looks promising
-        if (isSafe(grid, row, col, num))
-        {
-            // make tentative assignment
-            grid[row][col] = num;
-            
-            // return, if success, yay!
-            if (SolveSudoku(grid))
-                return true;
-            
-            // failure, unmake & try again
-            grid[row][col] = UNASSIGNED;
+    void genSubs(vector<int> nums, int start, vector<vector<int>>& sub, vector<int>& subs){
+        sub.push_back(subs);
+        for(int i=start; i<nums.size(); i++){
+            subs.push_back(nums[i]);
+            genSubs(nums, i+1, sub, subs);
+            subs.pop_back();
         }
     }
-    return false; // this triggers backtracking
-}
-
-/* Searches the grid to find an entry that is still unassigned. If
- found, the reference parameters row, col will be set the location
- that is unassigned, and true is returned. If no unassigned entries
- remain, false is returned. */
-bool FindUnassignedLocation(int grid[N][N], int &row, int &col)
-{
-    for (row = 0; row < N; row++)
-        for (col = 0; col < N; col++)
-            if (grid[row][col] == UNASSIGNED)
-                return true;
-    return false;
-}
-
-/* Returns a boolean which indicates whether any assigned entry
- in the specified row matches the given number. */
-bool UsedInRow(int grid[N][N], int row, int num)
-{
-    for (int col = 0; col < N; col++)
-        if (grid[row][col] == num)
-            return true;
-    return false;
-}
-
-/* Returns a boolean which indicates whether any assigned entry
- in the specified column matches the given number. */
-bool UsedInCol(int grid[N][N], int col, int num)
-{
-    for (int row = 0; row < N; row++)
-        if (grid[row][col] == num)
-            return true;
-    return false;
-}
-
-/* Returns a boolean which indicates whether any assigned entry
- within the specified 3x3 box matches the given number. */
-bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num)
-{
-    for (int row = 0; row < 3; row++)
-        for (int col = 0; col < 3; col++)
-            if (grid[row+boxStartRow][col+boxStartCol] == num)
-                return true;
-    return false;
-}
-
-/* Returns a boolean which indicates whether it will be legal to assign
- num to the given row,col location. */
-bool isSafe(int grid[N][N], int row, int col, int num)
-{
-    /* Check if 'num' is not already placed in current row,
-     current column and current 3x3 box */
-    return !UsedInRow(grid, row, num) &&
-    !UsedInCol(grid, col, num) &&
-    !UsedInBox(grid, row - row%3 , col - col%3, num);
-}
-
-/* A utility function to print grid  */
-void printGrid(int grid[N][N])
-{
-    for (int row = 0; row < N; row++)
-    {
-        for (int col = 0; col < N; col++)
-            printf("%2d", grid[row][col]);
-        printf("\n");
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> subs;
+        vector<vector<int>> sub;
+        sort(nums.begin(), nums.end());
+        genSubs(nums, 0, sub, subs);
+        return sub;
     }
-}
+    
+};
+
+
+
+
 
 /* Driver Program to test above functions */
 int main()
 {
-    // 0 means unassigned cells
-    int grid[N][N] = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
-        {5, 2, 0, 0, 0, 0, 0, 0, 0},
-        {0, 8, 7, 0, 0, 0, 0, 3, 1},
-        {0, 0, 3, 0, 1, 0, 0, 8, 0},
-        {9, 0, 0, 8, 6, 3, 0, 0, 5},
-        {0, 5, 0, 0, 9, 0, 6, 0, 0},
-        {1, 3, 0, 0, 0, 0, 2, 5, 0},
-        {0, 0, 0, 0, 0, 0, 0, 7, 4},
-        {0, 0, 5, 2, 0, 6, 3, 0, 0}};
-    if (SolveSudoku(grid) == true)
-        printGrid(grid);
-    else
-        printf("No solution exists");
+    Solution s;
+    vector<int> vec = vector<int>{1,2,3};
+
+    vector<vector<int>> result = s.subsets(vec);
+    
+    for(int i=0; i<result.size(); i++)
+    {
+        for(int j = 0; j<result[i].size(); j++)
+            cout << result[i][j];
+        cout << endl;
+    }
     
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+#if 0
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> result;
+    
+    sort(nums.begin(), nums.end());
+    
+    result.push_back(vector<int>{});
+    
+    for(int len = 1; len<=nums.size(); len++){
+        for(int i=0; i<nums.size() - len + 1; i++){
+            vector<int>::iterator first = nums.begin() + i;
+            vector<int>::iterator second = nums.begin() + i + len;
+            result.push_back(vector<int>(first, second));
+        }
+    }
+    
+    return result;
+}
+#endif
