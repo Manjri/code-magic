871ca804f11c3d18fcfbd2496192736f04b2ff87
njagdale <njagdale@gmail.com>
2015-11-05 20:46:37 -0800

    update

--- puzzles/Backtracking_NQueens/main.cpp
+++ puzzles/Backtracking_NQueens/main.cpp
@@ -1,6 +1,11 @@
 #define N 4
 #include<stdio.h>
 
+/**
+ The N Queen is the problem of placing N chess queens on an N×N chessboard 
+ so that no two queens attack each other.
+*/
+
 /* A utility function to print solution */
 void printSolution(int board[N][N])
 {
