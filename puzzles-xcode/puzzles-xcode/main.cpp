da1ec1eac9a36842cf474715b4d8badbf172ad8e
njagdale <njagdale@gmail.com>
2015-11-05 21:04:20 -0800

    update

--- puzzles-xcode/puzzles-xcode/main.cpp
+++ puzzles-xcode/puzzles-xcode/main.cpp
@@ -1,98 +1,56 @@
-// C program for Knight Tour problem
-#include<stdio.h>
-#define N 8
+#include <iostream>
+#include <vector>
+using namespace std;
 
-int solveKTUtil(int x, int y, int movei, int sol[N][N],
-                int xMove[],  int yMove[]);
-
-/* A utility function to check if i,j are valid indexes
- for N*N chessboard */
-bool isSafe(int x, int y, int sol[N][N])
+class Solution
 {
-    return ( x >= 0 && x < N && y >= 0 &&
-            y < N && sol[x][y] == -1);
-}
-
-/* A utility function to print solution matrix sol[N][N] */
-void printSolution(int sol[N][N])
-{
-    for (int x = 0; x < N; x++)
+public:
+    void moveZeroes(vector<int>& nums)
     {
-        for (int y = 0; y < N; y++)
-            printf(" %2d ", sol[x][y]);
-        printf("\n");
-    }
-}
-
-/* This function solves the Knight Tour problem using
- Backtracking.  This function mainly uses solveKTUtil()
- to solve the problem. It returns false if no complete
- tour is possible, otherwise return true and prints the
- tour.
- Please note that there may be more than one solutions,
- this function prints one of the feasible solutions.  */
-bool solveKT()
-{
-    int sol[N][N];
-    
-    /* Initialization of solution matrix */
-    for (int x = 0; x < N; x++)
-        for (int y = 0; y < N; y++)
-            sol[x][y] = -1;
-    
-    /* xMove[] and yMove[] define next move of Knight.
-     xMove[] is for next value of x coordinate
-     yMove[] is for next value of y coordinate */
-    int xMove[8] = {  2, 1, -1, -2, -2, -1,  1,  2 };
-    int yMove[8] = {  1, 2,  2,  1, -1, -2, -2, -1 };
-    
-    // Since the Knight is initially at the first block
-    sol[0][0]  = 0;
-    
-    /* Start from 0,0 and explore all tours using
-     solveKTUtil() */
-    if (solveKTUtil(0, 0, 1, sol, xMove, yMove) == false)
-    {
-        printf("Solution does not exist");
-        return false;
-    }
-    else
-        printSolution(sol);
-    
-    return true;
-}
-
-/* A recursive utility function to solve Knight Tour
- problem */
-int solveKTUtil(int x, int y, int movei, int sol[N][N],
-                int xMove[N], int yMove[N])
-{
-    int k, next_x, next_y;
-    if (movei == N*N)
-        return true;
-    
-    /* Try all next moves from the current coordinate x, y */
-    for (k = 0; k < 8; k++)
-    {
-        next_x = x + xMove[k];
-        next_y = y + yMove[k];
-        if (isSafe(next_x, next_y, sol))
+        int len = nums.size();
+        int last = 0;
+        int curr = 0;
+        int temp;
+        
+        if(len==0)
+            return;
+        
+        // last and curr are in stride for non-zero numbers
+        // when a zero is encountered last stops, while curr marches till next non-zero
+        // when curr finally reaches a non-zero, it swaps with last (which is at a zero position) and increments it
+        
+        while(curr<len)
         {
-            sol[next_x][next_y] = movei;
-            if (solveKTUtil(next_x, next_y, movei+1, sol,
-                            xMove, yMove) == true)
-                return true;
-            else
-                sol[next_x][next_y] = -1;// backtracking
+            if(nums[curr]!=0)
+            {
+                swap(nums[curr], nums[last]);
+                last++;
+            }
+            curr++;
         }
     }
-    
-    return false;
-}
+};
 
-/* Driver program to test above functions */
-int main()
-{
-    solveKT();
+int main(){
+    
+    vector<int> vec;
+    Solution s;
+    
+    vec.push_back(0);
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(0);
+    
+    for(int i=0; i<vec.size(); i++)
+        cout << vec[i] << " ";
+    
+    cout << endl;
+    
+    s.moveZeroes(vec);
+
+    for(int i=0; i<vec.size(); i++)
+        cout << vec[i] << " ";
+        
     return 0;
 }
--- puzzles/Array_MoveZeros/main.cpp
+++ puzzles/Array_MoveZeros/main.cpp
@@ -30,3 +30,27 @@ public:
         }
     }
 };
+
+int main(){
+    
+    vector<int> vec;
+    Solution s;
+    
+    vec.push_back(0);
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(0);
+    
+    for(int i=0; i<vec.size(); i++)
+        cout << vec[i] << " ";
+    
+    cout << endl;
+    
+    s.moveZeroes(vec);
+    
+    for(int i=0; i<vec.size(); i++)
+        cout << vec[i] << " ";
+    
+    return 0;
+}
