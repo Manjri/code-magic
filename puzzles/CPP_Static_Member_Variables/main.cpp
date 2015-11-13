f76d699dfc6a4ebd41c129e907efcef1ae728617
njagdale <njagdale@gmail.com>
2015-11-12 19:28:41 -0800

    update

--- puzzles/CPP_Static_Member_Variables/main.cpp
+++ puzzles/CPP_Static_Member_Variables/main.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
+#include "Something.h"
 
-class Something
-{
-private:
-    static int s_nIDGenerator;
-    int m_nID;
-public:
-    Something() { m_nID = s_nIDGenerator++; }
-    
-    int GetID() const { return m_nID; }
-};
-
-int Something::s_nIDGenerator = 1;
+// This works!
+const double Something::fudgeFactor = 1.57;
+int Something::s_nIDGenerator = 0;
 
 int main()
 {
     Something cFirst;
     Something cSecond;
     Something cThird;
-    
-    using namespace std;
-    cout << cFirst.GetID() << endl;
-    cout << cSecond.GetID() << endl;
-    cout << cThird.GetID() << endl;
+   
+    std::cout << cFirst.GetID() << std::endl;
+    std::cout << cSecond.GetID() << std::endl;
+    std::cout << cThird.GetID() << std::endl;
     return 0;
 }
